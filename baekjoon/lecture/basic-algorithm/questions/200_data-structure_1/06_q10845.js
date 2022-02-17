d796c59e844c7849cc2c81ce55e93b1c18d18ede
Haedong Kim <haedong1111@gmail.com>
2022-02-17 21:01:09 +0900

    modify lecture_basic-algorithm_200_06

--- baekjoon/lecture/basic-algorithm/questions/200_data-structure_1/06_q10845.js
+++ baekjoon/lecture/basic-algorithm/questions/200_data-structure_1/06_q10845.js
@@ -4,29 +4,78 @@ const input = fs.readFileSync("../input.txt").toString().trim().split("\n");
 input.shift();
 const commands = input.map((x) => x.split(/\s+/));
 
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.next = null;
+  }
+}
+class Queue {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.queueSize = 0;
+  }
+  push(data) {
+    let node = new Node(data);
+    if (!this.head) {
+      this.head = node;
+      this.head.next = this.tail;
+    } else {
+      this.tail.next = node;
+    }
+    this.tail = node;
+    this.queueSize++;
+  }
+  pop() {
+    if (!this.queueSize) return -1;
+    const popped = this.head.data;
+    if (this.head === this.tail) {
+      this.head = null;
+      this.tail = null;
+    } else {
+      this.head = this.head.next;
+    }
+    this.queueSize--;
+    return popped;
+  }
+  size() {
+    return this.queueSize;
+  }
+  empty() {
+    return this.queueSize ? 0 : 1;
+  }
+  front() {
+    return this.queueSize ? this.head.data : -1;
+  }
+  back() {
+    return this.queueSize ? this.tail.data : -1;
+  }
+}
+
 const solution = (commands) => {
-  let queue = [],
-    result = "";
+  let result = "";
+  let queue = new Queue();
 
   commands.forEach((command) => {
     switch (command[0]) {
       case "push":
-        queue[queue.length] = command[1];
+        queue.push(command[1]);
         break;
       case "pop":
-        result += (queue.length ? queue.splice(0, 1)[0] : "-1") + "\n";
+        result += queue.pop() + "\n";
         break;
       case "size":
-        result += `${queue.length}\n`;
+        result += queue.size() + "\n";
         break;
       case "empty":
-        result += (queue.length ? "0" : "1") + "\n";
+        result += queue.empty() + "\n";
         break;
       case "front":
-        result += (queue.length ? queue[0] : "-1") + "\n";
+        result += queue.front() + "\n";
         break;
       case "back":
-        result += (queue.length ? queue[queue.length - 1] : "-1") + "\n";
+        result += queue.back() + "\n";
         break;
       default:
         break;
