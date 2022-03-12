18485df2a6ad19c0256a9f49836b9991d328b3a1
Haedong Kim <haedong1111@gmail.com>
2022-03-12 22:07:49 +0900

    lecture_basic-algorithm_510_brute-force_03

---
+++ baekjoon/lecture/basic-algorithm/questions/510_brute-force/04_q15652.js
@@ -0,0 +1,40 @@
+const fs = require("fs");
+const [n, m] = fs
+  .readFileSync("../input.txt")
+  .toString()
+  .trim()
+  .split(/\s+/)
+  .map(Number);
+
+printPermutationWithRepetitionOneToN(n, m);
+
+function printPermutationWithRepetitionOneToN(n, r) {
+  const permutation = getPermutationWithRepetitionOneToN(n, r).map((v) =>
+    v.sort((a, b) => a - b).join(" ")
+  );
+  console.log([...new Set(permutation)].join("\n"));
+}
+
+function getPermutationWithRepetitionOneToN(n, r) {
+  const numArr = new Array(n).fill().map((v, i) => (v = i + 1));
+  return getPermutationWithRepetition(numArr, r);
+}
+
+function getPermutationWithRepetition(numArr, r) {
+  let result = [];
+  if (r === 1) {
+    return numArr.map((v) => [v]);
+  }
+
+  numArr.forEach((fixed, idx, ori) => {
+    const rest = ori;
+    const permutationOfRest = getPermutationWithRepetition(rest, r - 1);
+    const part = permutationOfRest.map((permutation) => [
+      fixed,
+      ...permutation,
+    ]);
+    result.push(...part);
+  });
+
+  return result;
+}
