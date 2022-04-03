708ae2092dbad9c6b3ff15e8e8d5e56cc4458d80
Haedong Kim <haedong1111@gmail.com>
2022-04-03 14:16:05 +0900

    lecture-basic_algorithm-600_graph_1-07_bfs_refactor

--- baekjoon/lecture/basic_algorithm/questions/600_gragh_1/07_q2178.js
+++ baekjoon/lecture/basic_algorithm/questions/600_gragh_1/07_q2178.js
@@ -9,24 +9,23 @@ console.log(getMinPath(maze, width, height));
 function getMinPath(maze, width, height) {
   const visited = Array.from(Array(height), () => new Array(width));
   const AVAILABLE = '1';
-  const dx = [0, 0, -1, 1];
-  const dy = [-1, 1, 0, 0];
 
   bfs([0, 0]);
 
   return visited[height - 1][width - 1];
 
   function bfs([x, y]) {
-    let xCur, yCur, xNext, yNext;
+    const dx = [0, 0, -1, 1];
+    const dy = [-1, 1, 0, 0];
     const queue = [[x, y]];
     visited[y][x] = 1;
 
     while (queue.length) {
-      [xCur, yCur] = queue.shift();
+      const [xCur, yCur] = queue.shift();
 
       for (let i = 0; i < 4; i++) {
-        xNext = xCur + dx[i];
-        yNext = yCur + dy[i];
+        const xNext = xCur + dx[i];
+        const yNext = yCur + dy[i];
         if (xNext >= 0 && yNext >= 0 && xNext < width && yNext < height) {
           if (maze[yNext][xNext] === AVAILABLE && !visited[yNext][xNext]) {
             visited[yNext][xNext] = visited[yCur][xCur] + 1;
