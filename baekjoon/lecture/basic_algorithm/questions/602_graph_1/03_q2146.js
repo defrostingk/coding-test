70add6c39d23115ff5f6fab10508b4d2c67d087b
Haedong Kim <haedong1111@gmail.com>
2022-04-10 23:27:00 +0900

    lecture-basic_algorithm-602_graph_1-03_refactor

--- baekjoon/lecture/basic_algorithm/questions/602_graph_1/03_q2146.js
+++ baekjoon/lecture/basic_algorithm/questions/602_graph_1/03_q2146.js
@@ -4,7 +4,6 @@ const input = fs.readFileSync('../input.txt').toString().trim().split('\n');
 const mapSize = +input[0];
 const map = input.slice(1).map((column) => column.split(' '));
 const ISLAND = '1';
-const SEA = '0';
 
 console.log(getShortestBridge(map, mapSize));
 
@@ -12,24 +11,27 @@ function getShortestBridge(map, mapSize) {
   const dx = [0, 0, -1, 1];
   const dy = [-1, 1, 0, 0];
   const bridges = [];
-  let checked = Array.from(Array(mapSize), () => new Array(mapSize));
+  const checked = Array.from(Array(mapSize), () => new Array(mapSize));
+  const visited = Array.from(Array(mapSize), () => new Array(mapSize));
+  let id = 1;
 
   for (let y = 0; y < mapSize; y++) {
     for (let x = 0; x < mapSize; x++) {
       if (map[y][x] === ISLAND && !checked[y][x]) {
-        checked = checkIsland([x, y]);
-        bfs([x, y], checked);
+        checkIsland([x, y], (id++).toString());
       }
     }
   }
 
+  getVisited();
+  getDistance();
+
   return Math.min(...bridges);
 
-  function checkIsland([x, y]) {
-    const checked = Array.from(Array(mapSize), () => new Array(mapSize));
+  function checkIsland([x, y], id) {
     const queue = [[x, y]];
     let head = 0;
-    checked[y][x] = 1;
+    checked[y][x] = id;
 
     while (queue.length > head) {
       const [xCur, yCur] = queue[head++];
@@ -38,23 +40,23 @@ function getShortestBridge(map, mapSize) {
         if (isInRange(xNext, yNext)) {
           if (!checked[yNext][xNext] && isIsland(xNext, yNext)) {
             queue.push([xNext, yNext]);
-            checked[yNext][xNext] = 1;
+            checked[yNext][xNext] = id;
           }
         }
       }
     }
-
-    return checked;
   }
 
-  function bfs([x, y], checked) {
-    const visited = Array.from(Array(mapSize), () => new Array(mapSize));
-    const queue = [[x, y]];
+  function getVisited() {
+    const queue = [];
     let head = 0;
 
-    for (let j = 0; j < mapSize; j++) {
-      for (let i = 0; i < mapSize; i++) {
-        visited[j][i] = checked[j][i];
+    for (let y = 0; y < mapSize; y++) {
+      for (let x = 0; x < mapSize; x++) {
+        if (checked[y][x]) {
+          queue.push([x, y]);
+          visited[y][x] = 1;
+        }
       }
     }
 
@@ -63,13 +65,26 @@ function getShortestBridge(map, mapSize) {
       for (let i = 0; i < dx.length; i++) {
         const [xNext, yNext] = [xCur + dx[i], yCur + dy[i]];
         if (isInRange(xNext, yNext)) {
-          if (!visited[yNext][xNext]) {
+          if (!checked[yNext][xNext]) {
             queue.push([xNext, yNext]);
+            checked[yNext][xNext] = checked[yCur][xCur];
             visited[yNext][xNext] = visited[yCur][xCur] + 1;
-            if (seaToIsland(xCur, yCur, xNext, yNext)) {
-              const length = visited[yNext][xNext] - visited[y][x] - 1;
-              bridges.push(length);
-              return;
+          }
+        }
+      }
+    }
+  }
+
+  function getDistance() {
+    for (let y = 0; y < mapSize; y++) {
+      for (let x = 0; x < mapSize; x++) {
+        for (let i = 0; i < dx.length; i++) {
+          const nx = x + dx[i];
+          const ny = y + dy[i];
+          if (isInRange(nx, ny)) {
+            if (checked[ny][nx] !== checked[y][x]) {
+              const distance = visited[y][x] + visited[ny][nx] - 2;
+              bridges.push(distance);
             }
           }
         }
@@ -82,8 +97,5 @@ function getShortestBridge(map, mapSize) {
   }
   function isIsland(x, y) {
     return map[y][x] === ISLAND;
-  }
-  function seaToIsland(xCur, yCur, xNext, yNext) {
-    return map[yCur][xCur] === SEA && map[yNext][xNext] === ISLAND;
   }
 }
