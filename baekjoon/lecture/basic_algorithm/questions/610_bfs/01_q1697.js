c8802ece4e31329265361c71d4954aa078fbd232
Haedong Kim <haedong1111@gmail.com>
2022-04-11 14:53:14 +0900

    lecture-basic_algorithm-610_bfs-01

---
+++ baekjoon/lecture/basic_algorithm/questions/610_bfs/01_q1697.js
@@ -0,0 +1,36 @@
+const fs = require('fs');
+const [n, k] = fs
+  .readFileSync('../input.txt')
+  .toString()
+  .split(' ')
+  .map(Number);
+const MAX = 100000;
+
+console.log(getShortestTime(n, k));
+
+function getShortestTime(n, k) {
+  if (n === k) return 0;
+  const visited = new Array(MAX + 1);
+  const queue = [n];
+  let head = 0;
+
+  visited[n] = 1;
+
+  while (queue.length > head) {
+    const current = queue[head++];
+
+    for (let next of [current - 1, current + 1, current * 2]) {
+      if (isAvailable(next)) {
+        if (next === k) return visited[current];
+        queue.push(next);
+        visited[next] = visited[current] + 1;
+      }
+    }
+  }
+
+  return -1;
+
+  function isAvailable(num) {
+    return num >= 0 && num < MAX + 1 && !visited[num];
+  }
+}
