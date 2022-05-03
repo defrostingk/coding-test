60bbb261f30aeb4a983eea7bb9a88ddd33d73123
Haedong Kim <haedong1111@gmail.com>
2022-05-03 17:53:14 +0900

    lecture-intermediate_algorithm-800_divide_and_conquer-03_two_pointer

--- baekjoon/lecture/intermediate_algorithm/800_divide_and_conquer/03_q11728.js
+++ baekjoon/lecture/intermediate_algorithm/800_divide_and_conquer/03_q11728.js
@@ -4,8 +4,19 @@ const input = fs.readFileSync('../input.txt').toString().trim().split('\n');
 solution(input);
 
 function solution(input) {
+  const [n, m] = input[0].split(' ').map(Number);
   const arrA = input[1].split(' ').map(Number);
   const arrB = input[2].split(' ').map(Number);
 
-  console.log([...arrA, ...arrB].sort((a, b) => a - b).join(' '));
+  const result = new Array(n + m);
+  let idxA = 0;
+  let idxB = 0;
+  for (let i = 0; i < result.length; i++) {
+    if (arrA[idxA] >= arrB[idxB]) result[i] = arrB[idxB++];
+    else if (idxA === n) result[i] = arrB[idxB++];
+    else if (arrA[idxA] < arrB[idxB]) result[i] = arrA[idxA++];
+    else if (idxB === m) result[i] = arrA[idxA++];
+  }
+
+  console.log(result.join(' '));
 }
