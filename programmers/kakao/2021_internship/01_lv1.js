057a157092f26a454820544e82422c85de3c4b81
Haedong Kim <haedong1111@gmail.com>
2022-05-04 14:11:18 +0900

    programmers-kakao-2021_internship-01

---
+++ programmers/kakao/2021_internship/01_lv1.js
@@ -0,0 +1,27 @@
+const fs = require('fs');
+const s = fs.readFileSync('01.txt').toString().trim();
+
+function solution(s) {
+  var answer = 0;
+
+  const numbers = [
+    'zero',
+    'one',
+    'two',
+    'three',
+    'four',
+    'five',
+    'six',
+    'seven',
+    'eight',
+    'nine',
+  ];
+  answer = s;
+  numbers.forEach((number, idx) => {
+    const reg = new RegExp(number, 'g');
+    answer = answer.replace(reg, `${idx}`);
+  });
+
+  return answer;
+}
+console.log(solution(s));
