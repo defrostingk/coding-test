5310227e0c46131a013acfdb785de1669805e1bc
Haedong Kim <haedong1111@gmail.com>
2022-05-07 20:21:13 +0900

    lecture-intermediate_algorithm-801_divide_and_conquer-03

---
+++ baekjoon/lecture/intermediate_algorithm/801_divide_and_conquer/03_q2263.js
@@ -0,0 +1,26 @@
+const fs = require('fs');
+const input = fs.readFileSync('../input.txt').toString().trim().split('\n');
+
+(function solution(input) {
+  const n = +input[0];
+  const [inorder, postorder] = input.slice(1).map((order) => order.split(' '));
+  const result = [];
+
+  const inorderIdx = new Array(n + 1);
+  inorder.forEach((node, idx) => (inorderIdx[node] = idx));
+
+  preorder(0, n - 1, 0, n - 1);
+
+  console.log(result.join(' '));
+
+  function preorder(inStart, inEnd, postStart, postEnd) {
+    if (inStart > inEnd || postStart > postEnd) return;
+    const root = postorder[postEnd];
+    const rootIdx = inorderIdx[root];
+    const left = rootIdx - inStart;
+    result.push(root);
+
+    preorder(inStart, rootIdx - 1, postStart, postStart + left - 1);
+    preorder(rootIdx + 1, inEnd, postStart + left, postEnd - 1);
+  }
+})(input);
