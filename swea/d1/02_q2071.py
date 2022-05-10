b907706bd2df11d09662cddb75853a427c59397d
Haedong Kim <haedong1111@gmail.com>
2022-05-10 11:53:27 +0900

    swea-d1-02_q2071

---
+++ swea/d1/02_q2071.py
@@ -0,0 +1,15 @@
+from sys import stdin
+
+stdin = open('/mnt/repository/coding-test/swea/input.txt', 'r')
+
+next(stdin)
+
+for idx, line in enumerate(stdin):
+    nums = map(int, line.split())
+    average = 0
+    for num in nums:
+        average += float(num)
+    average /= len(nums)
+    average = int(round(average))
+
+    print('#{0} {1}'.format(idx+1, average))
