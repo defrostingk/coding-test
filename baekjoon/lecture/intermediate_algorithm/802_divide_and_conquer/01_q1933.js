eeb237456ad7a39bc9f27ffb6630aed2113b6074
Haedong Kim <haedong1111@gmail.com>
2022-05-13 01:29:27 +0900

    lecture-intermediate_algorithm-802_divide_and_conquer-01_merge

--- baekjoon/lecture/intermediate_algorithm/802_divide_and_conquer/01_q1933.js
+++ baekjoon/lecture/intermediate_algorithm/802_divide_and_conquer/01_q1933.js
@@ -2,87 +2,82 @@ const fs = require('fs');
 const input = fs.readFileSync('../input.txt').toString().trim().split('\n');
 
 (function solution(input) {
-  class PriorityQueue {
-    constructor() {
-      this.heap = [];
-      this.size = 0;
-    }
+  const n = +input[0];
+  const buildings = input
+    .slice(1)
+    .map((building) => {
+      const [left, height, right] = building.split(' ').map(Number);
+      return { left, right, height };
+    })
+    .sort((a, b) => (a.left > b.left ? 1 : -1));
 
-    enqueue(building) {
-      this.size++;
-      let idx = this.size;
+  const skyLine = getSkyLine(0, n - 1);
 
-      while (idx !== 1) {
-        const parentIdx = Math.floor(idx / 2);
-        if (this.heap[parentIdx].x >= building.x) {
-          this.heap[idx] = this.heap[parentIdx];
-          idx = parentIdx;
-        } else break;
+  const answer = [];
+  skyLine.forEach(({ x, height }) => answer.push(x, height));
+  console.log(answer.join(' '));
+
+  function merge(left, right) {
+    let leftIdx = 0;
+    let rightIdx = 0;
+    let leftHeight = 0;
+    let rightHeight = 0;
+
+    const result = [];
+    while (leftIdx < left.length && rightIdx < right.length) {
+      const leftBuilding = left[leftIdx];
+      const rightBuilding = right[rightIdx];
+      let x, height;
+
+      if (leftBuilding.x >= rightBuilding.x) {
+        x = rightBuilding.x;
+        rightHeight = rightBuilding.height;
+        height = Math.max(leftHeight, rightHeight);
+        rightIdx++;
+      } else {
+        x = leftBuilding.x;
+        leftHeight = leftBuilding.height;
+        height = Math.max(leftHeight, rightHeight);
+        leftIdx++;
       }
 
-      this.heap[idx] = building;
-    }
-
-    dequeue() {
-      if (this.isEmpty()) return;
-      let popped = this.heap[1];
-      let tmp = this.heap.pop();
-      this.heap[1] = tmp;
-      this.size--;
-
-      let parent = 1;
-      let child = 2;
-
-      while (child <= this.size) {
-        if (
-          this.heap[child + 1] &&
-          this.heap[child].x > this.heap[child + 1].x &&
-          child < this.size
-        ) {
-          child++;
+      if (result.length > 0) {
+        if (height === result[result.length - 1].height) {
+          continue;
         }
-        if (tmp.x <= this.heap[child].x) break;
-        this.heap[parent] = this.heap[child];
-        parent = child;
-        child *= 2;
+        if (x === result[result.length - 1].x) {
+          result[result.length - 1].height = height;
+          continue;
+        }
       }
-      this.heap[parent] = tmp;
-
-      return popped;
+      result.push({ x, height });
     }
 
-    isEmpty() {
-      return !this.size;
+    while (leftIdx < left.length) {
+      result.push(left[leftIdx]);
+      leftIdx++;
     }
+
+    while (rightIdx < right.length) {
+      result.push(right[rightIdx]);
+      rightIdx++;
+    }
+
+    return result;
   }
 
-  const pq = new PriorityQueue();
-  input.slice(1).forEach((building) => {
-    const [left, height, right] = building.split(' ').map(Number);
-    pq.enqueue({ x: left, height });
-    pq.enqueue({ x: right, height: -height });
-  });
-
-  const heights = new Map();
-  const skyline = [];
-
-  let preHeight = 0;
-  let maxHeight = 0;
-  while (!pq.isEmpty()) {
-    const { x, height } = pq.dequeue();
-    if (height > 0) {
-      heights.set(height, x);
-      if (maxHeight < height) maxHeight = height;
-    } else {
-      heights.delete(-height);
-      maxHeight = heights.size ? Math.max(...heights.keys()) : 0;
+  function getSkyLine(start, end) {
+    if (start === end) {
+      return [
+        { x: buildings[start].left, height: buildings[start].height },
+        { x: buildings[start].right, height: 0 },
+      ];
     }
 
-    if (preHeight !== maxHeight) {
-      skyline.push(x, maxHeight);
-      preHeight = maxHeight;
-    }
+    const mid = Math.floor((start + end) / 2);
+    const left = getSkyLine(start, mid);
+    const right = getSkyLine(mid + 1, end);
+
+    return merge(left, right);
   }
-
-  console.log(skyline.join(' '));
 })(input);
