5c3f5ee23118392b486532520c742ba773078800
Haedong Kim <haedong1111@gmail.com>
2022-05-27 11:31:38 +0900

    lecture-intermediate_algorithm-541_brute_force-03_dfs

---
+++ baekjoon/lecture/intermediate_algorithm/541_brute_force/03_12100.js
@@ -0,0 +1,123 @@
+const fs = require('fs');
+const input = fs.readFileSync('../input.txt').toString().trim().split('\n');
+
+(function solution(input) {
+  const n = +input[0];
+  const oriBoard = input.slice(1).map((row) => row.split(' ').map(Number));
+  const [UP, DOWN, LEFT, RIGHT] = [0, 1, 2, 3];
+
+  let maxBlock = 0;
+  dfs(oriBoard, 0);
+
+  console.log(maxBlock);
+
+  function dfs(board, cnt) {
+    if (cnt >= 5) {
+      maxBlock = Math.max(maxBlock, Math.max(...board.flat()));
+      return;
+    }
+    for (let dir = 0; dir < 4; dir++) {
+      const nextBoard = Array.from(Array(n), () => new Array(n));
+      for (let j = 0; j < n; j++) {
+        for (let i = 0; i < n; i++) {
+          nextBoard[j][i] = board[j][i];
+        }
+      }
+      moveBlocks(nextBoard, dir);
+      dfs(nextBoard, cnt + 1);
+    }
+  }
+  function moveBlocks(board, dir) {
+    const queue = [];
+    switch (dir) {
+      case UP:
+        for (let x = 0; x < n; x++) {
+          for (let y = 0; y < n; y++) {
+            if (board[y][x]) {
+              queue.push(board[y][x]);
+              board[y][x] = 0;
+            }
+          }
+          let tmp;
+          let idx = 0;
+          while (queue.length) {
+            tmp = queue.shift();
+            if (!board[idx][x]) board[idx][x] = tmp;
+            else if (board[idx][x] === tmp) {
+              board[idx][x] *= 2;
+              idx++;
+            } else {
+              board[++idx][x] = tmp;
+            }
+          }
+        }
+        break;
+      case DOWN:
+        for (let x = 0; x < n; x++) {
+          for (let y = n - 1; y >= 0; y--) {
+            if (board[y][x]) {
+              queue.push(board[y][x]);
+              board[y][x] = 0;
+            }
+          }
+          let tmp;
+          let idx = n - 1;
+          while (queue.length) {
+            tmp = queue.shift();
+            if (!board[idx][x]) board[idx][x] = tmp;
+            else if (board[idx][x] === tmp) {
+              board[idx][x] *= 2;
+              idx--;
+            } else {
+              board[--idx][x] = tmp;
+            }
+          }
+        }
+        break;
+      case LEFT:
+        for (let y = 0; y < n; y++) {
+          for (let x = 0; x < n; x++) {
+            if (board[y][x]) {
+              queue.push(board[y][x]);
+              board[y][x] = 0;
+            }
+          }
+          let tmp;
+          let idx = 0;
+          while (queue.length) {
+            tmp = queue.shift();
+            if (!board[y][idx]) board[y][idx] = tmp;
+            else if (board[y][idx] === tmp) {
+              board[y][idx] *= 2;
+              idx++;
+            } else {
+              board[y][++idx] = tmp;
+            }
+          }
+        }
+        break;
+      case RIGHT:
+        for (let y = 0; y < n; y++) {
+          for (let x = n - 1; x >= 0; x--) {
+            if (board[y][x]) {
+              queue.push(board[y][x]);
+              board[y][x] = 0;
+            }
+          }
+          let tmp;
+          let idx = n - 1;
+          while (queue.length) {
+            tmp = queue.shift();
+            if (!board[y][idx]) board[y][idx] = tmp;
+            else if (board[y][idx] === tmp) {
+              board[y][idx] *= 2;
+              idx--;
+            } else {
+              board[y][--idx] = tmp;
+            }
+          }
+        }
+        break;
+    }
+  }
+})(input);
